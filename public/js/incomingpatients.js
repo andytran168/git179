fa855fc51e459f33f3f3fa74ac172dde4909ebff
ALVIN W <alvinwei3@gmail.com>
2016-04-22 00:46:08 -0400

    changed arrival distance to time in minutes

--- public/js/incomingpatients.js
+++ public/js/incomingpatients.js
@@ -1,7 +1,7 @@
 function populatePage() {
   if(sessionStorage.getItem('user')) {
     var user = JSON.parse(sessionStorage.getItem('user'));
-    var update = '<li class="ui-li-has-thumb ui-first-child"><a href="/patientinfo" class="ui-btn ui-btn-icon-right ui-icon-carat-r"><img src="img/ph.png" alt="Image"><h2>' + user.name + '</h2><p>1.6 mile</p></a></li>' + document.getElementById('patientlist').innerHTML;
+    var update = '<li class="ui-li-has-thumb ui-first-child"><a href="/patientinfo" class="ui-btn ui-btn-icon-right ui-icon-carat-r"><img src="img/ph.png" alt="Image"><h2>' + user.name + '</h2><p>6 minutes</p></a></li>' + document.getElementById('patientlist').innerHTML;
     document.getElementById('patientlist').innerHTML = update;
   }
 }
